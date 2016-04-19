2a85e17d774e4d2a75f69f29c5c3e78db182cb99
psw7194 <psw7194@hanmail.net>
2016-04-20 01:18:43 +0900

    design

--- public/javascripts/daummaps.js
+++ public/javascripts/daummaps.js
@@ -15,22 +15,55 @@ function renderDaumMap(mapIdx) {
     var container = document.getElementById('daum_map');
     var options = {
         center: new daum.maps.LatLng(wLatArr[mapIdx], lLatArr[mapIdx]),
-        level: 3
+        level: 2
     };
     //
     var map = new daum.maps.Map(container, options);
 
-    // 마커가 표시될 위치입니다 
-    var markerPosition  = new daum.maps.LatLng(wLatArr[mapIdx], lLatArr[mapIdx]); 
+    var imageSrc = 'http://localhost:3000/images/map/marker_roost.png', // 마커이미지의 주소입니다    
+        imageSize = new daum.maps.Size(64, 69), // 마커이미지의 크기입니다
+        imageOprion = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize, imageOprion),
+        markerPosition = new daum.maps.LatLng(wLatArr[mapIdx], lLatArr[mapIdx]); // 마커가 표시될 위치입니다
+    
     // 마커를 생성합니다
     var marker = new daum.maps.Marker({
-        position: markerPosition
+        position: markerPosition,
+        image: markerImage // 마커이미지 설정 
     });
+
+    // 마커가 표시될 위치입니다 
+    //var markerPosition  = new daum.maps.LatLng(wLatArr[mapIdx], lLatArr[mapIdx]); 
+
+    // 마커를 생성합니다
+    //var marker = new daum.maps.Marker({
+    //    position: markerPosition
+    //});
 
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);
 
+    // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    var content = '<div class="customoverlay">' +
+        '  <a href="http://map.daum.net/link/map/' + itemIdArr[mapIdx] + '" target="_blank">' +
+        '    <span class="title">' + storeNmArr[mapIdx] + '</span>' +
+        '  </a>' +
+        '</div>';
+    
+    // 커스텀 오버레이가 표시될 위치입니다 
+    var position = new daum.maps.LatLng(wLatArr[mapIdx], lLatArr[mapIdx]);  
+
+    // 커스텀 오버레이를 생성합니다
+    var customOverlay = new daum.maps.CustomOverlay({
+        map: map,
+        position: position,
+        content: content,
+        yAnchor: 1 
+    });
+
+/*
     var conTemp = "";
     conTemp += '<div style="padding:5px;">' + storeNmArr[mapIdx];
     conTemp += '  <br>';
@@ -48,4 +81,5 @@ function renderDaumMap(mapIdx) {
     
     // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
     infowindow.open(map, marker); 
+*/
 }

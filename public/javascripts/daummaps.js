99f9dabbf5e94eb51b39215b06d668fabb97160b
psw7194 <psw7194@hanmail.net>
2016-04-20 01:25:19 +0900

    map url change

--- public/javascripts/daummaps.js
+++ public/javascripts/daummaps.js
@@ -20,7 +20,7 @@ function renderDaumMap(mapIdx) {
     //
     var map = new daum.maps.Map(container, options);
 
-    var imageSrc = 'http://localhost:3000/images/map/marker_roost.png', // 마커이미지의 주소입니다    
+    var imageSrc = 'http://psw7194.iptime.org/images/map/marker_roost.png', // 마커이미지의 주소입니다    
         imageSize = new daum.maps.Size(64, 69), // 마커이미지의 크기입니다
         imageOprion = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
